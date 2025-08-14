3b5318f1eb24b557a02b7d3a759dee3a62f38fb4
jonathan <jonathhu@stud.ntnu.no>
2025-08-14 22:40:52 +0200

    Add gtkmm-example

---
+++ oving4/build/CMakeFiles/oving4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oving4.dir/main.cpp.o"
+  "CMakeFiles/oving4.dir/main.cpp.o.d"
+  "oving4"
+  "oving4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oving4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ oving4/build/CMakeFiles/oving4.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for oving4.
+# This may be replaced when dependencies are built.
