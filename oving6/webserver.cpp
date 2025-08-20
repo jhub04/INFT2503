83d78fb48a863d796de0ae59f26167c6bdc8cccb
jonathan <jonathhu@stud.ntnu.no>
2025-08-20 11:56:22 +0200

    Create appropriate http response and send to client buffer

--- oving6/webserver.cpp
+++ oving6/webserver.cpp
@@ -17,7 +17,7 @@ private:
   tcp::endpoint endpoint;
   tcp::acceptor acceptor;
 
-  string getHTTPPath(string message) {
+  string get_http_path(string message) {
     istringstream iss(message);
 
     string httpMethod;
@@ -27,6 +27,24 @@ private:
     iss >> httpMethod >> httpPath >> httpVersion;
 
     return httpPath;
+  }
+
+  string make_http_response(int status_code, const string &status_message, const string &body) {
+    string status_line = "HTTP/1.1 " + to_string(status_code) + " " + status_message + "\r\n";
+    string headers = "Content-Type: text/plain\r\n"
+                     "Content-Length: " + to_string(body.size()) + "\r\n\r\n";
+
+    return status_line + headers + body;
+  }
+
+  string route(string path) {
+    if (path == "/") {
+      return make_http_response(200, "OK", "Dette er hjemmesiden");
+    } else if (path == "/en_side") {
+      return make_http_response(200, "OK", "Dette er en side");
+    } else {
+      return make_http_response(404, "Not Found", "404 Not Found");
+    }
   }
 
   void handle_request(shared_ptr<Connection> connection) {
@@ -41,8 +59,9 @@ private:
         getline(read_stream, message);
         message.pop_back(); // Remove "\r" at the end of message
 
-        string path = getHTTPPath(message);
+        string path = get_http_path(message);
 
+        string http_response = route(path);
 
         // Close connection when "exit" is retrieved from client
         if (message == "exit")
@@ -54,7 +73,7 @@ private:
         ostream write_stream(write_buffer.get());
 
         // Add message to be written to client:
-        write_stream << message << "\r\n";
+        write_stream << http_response << "\r\n";
 
         // Write to client
         async_write(connection->socket, *write_buffer, [this, connection, write_buffer](const boost::system::error_code &ec, size_t) {
